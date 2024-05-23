ca2c4e2c4a73e0c185c89879fe5cd16203b7dcab
Frank Kloeker <f.kloeker@telekom.de>
2024-05-23 20:07:05 +0200

    fix expire days

--- tagexpire.py
+++ tagexpire.py
@@ -8,7 +8,7 @@ username = sys.argv[1]
 token = sys.argv[2]
 repo_type = sys.argv[3]
 image_name = sys.argv[4]
-days_threshold = sys.argv[5] if len(sys.argv) > 5 else None
+days_threshold = int(sys.argv[5])
 orgname = sys.argv[6] if len(sys.argv) > 6 else None
 
 # Basic auth using your username and PAT
