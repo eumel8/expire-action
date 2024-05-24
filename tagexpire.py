6ea2931766f50351fcb5b8fc2b13e6723836f4b6
Frank Kloeker <f.kloeker@telekom.de>
2024-05-24 09:25:01 +0200

    query 100 per page and round up loops

--- tagexpire.py
+++ tagexpire.py
@@ -2,6 +2,7 @@ import requests
 from requests.exceptions import HTTPError
 from datetime import datetime, timedelta
 import json
+import math
 import sys
 
 # define command line arguments username, token, image_name, days_threshold, orgname
@@ -24,27 +25,27 @@ if repo_type == 'user':
         response = requests.get(f'https://api.github.com/user/packages/container/{image_name}', auth=auth)
         response.raise_for_status()
         versions = response.json()
-        version_count = int(versions['version_count'])
+        version_count = math.ceil(int(versions['version_count'])//100 + 1)
         print("version count ", version_count)
         # loop through the versions and check if the created_at is older than the threshold
         for i in range(1, version_count + 1):
-            response = requests.get(f'https://api.github.com/user/packages/container/{image_name}/versions?per_page=1&page={i}', auth=auth)
+            response = requests.get(f'https://api.github.com/user/packages/container/{image_name}/versions?per_page=100&page={i}', auth=auth)
             tags = response.json()
-            tag = tags[0]
-            created_at = datetime.strptime(tag['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-            if created_at < threshold_date:
-                print(f'Delete tag {tag["id"]} - {tag["name"]}')
-                try:
-                    response = requests.delete(f'https://api.github.com/user/packages/container/{image_name}/versions/{tag["id"]}', auth=auth)
-                    response.raise_for_status()
-                except HTTPError as e:
-                    if response.status_code == 404:
-                        print("The requested resource was not found.")
-                    elif response.status_code == 500:
-                        print("The server encountered an internal error.")
-                    else:
-                        print(f"HTTP error occurred: {e}")
-                        print(f"Response message: {response.text}")
+            for tag in tags:
+                created_at = datetime.strptime(tag['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+                if created_at < threshold_date:
+                    print(f'Delete tag {tag["id"]} - {tag["name"]}')
+                    try:
+                        response = requests.delete(f'https://api.github.com/user/packages/container/{image_name}/versions/{tag["id"]}', auth=auth)
+                        response.raise_for_status()
+                    except HTTPError as e:
+                        if response.status_code == 404:
+                            print("The requested resource was not found.")
+                        elif response.status_code == 500:
+                            print("The server encountered an internal error.")
+                        else:
+                            print(f"HTTP error occurred: {e}")
+                            print(f"Response message: {response.text}")
     except HTTPError as e:
         if response.status_code == 404:
             print("The requested resource was not found.")
@@ -59,27 +60,27 @@ elif repo_type == 'org':
         response = requests.get(f'https://api.github.com/orgs/{orgname}/packages/container/{image_name}', auth=auth)
         response.raise_for_status()
         versions = response.json()
-        version_count = int(versions['version_count'])
+        version_count = math.ceil(int(versions['version_count'])//100 + 1)
         print("version count ",version_count)
         # loop through the versions and check if the created_at is older than the threshold
         for i in range(1, version_count + 1):
-            response = requests.get(f'https://api.github.com/orgs/{orgname}/packages/container/{image_name}/versions?per_page=1&page={i}', auth=auth)
+            response = requests.get(f'https://api.github.com/orgs/{orgname}/packages/container/{image_name}/versions?per_page=100&page={i}', auth=auth)
             tags = response.json()
-            tag = tags[0]
-            created_at = datetime.strptime(tag['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-            if created_at < threshold_date:
-                print(f'Delete tag {tag["id"]} - {tag["name"]}')
-                try:
-                    response = requests.delete(f'https://api.github.com/orgs/{orgname}/packages/container/{image_name}/versions/{tag["id"]}', auth=auth)
-                    response.raise_for_status()
-                except HTTPError as e:
-                    if response.status_code == 404:
-                        print("The requested resource was not found.")
-                    elif response.status_code == 500:
-                        print("The server encountered an internal error.")
-                    else:
-                        print(f"HTTP error occurred: {e}")
-                        print(f"Response message: {response.text}")
+            for tag in tags:
+                created_at = datetime.strptime(tag['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+                if created_at < threshold_date:
+                    print(f'Delete tag {tag["id"]} - {tag["name"]}')
+                    try:
+                        response = requests.delete(f'https://api.github.com/orgs/{orgname}/packages/container/{image_name}/versions/{tag["id"]}', auth=auth)
+                        response.raise_for_status()
+                    except HTTPError as e:
+                        if response.status_code == 404:
+                            print("The requested resource was not found.")
+                        elif response.status_code == 500:
+                            print("The server encountered an internal error.")
+                        else:
+                            print(f"HTTP error occurred: {e}")
+                            print(f"Response message: {response.text}")
 
     except HTTPError as e:
         if response.status_code == 404:
